8453e586d271c427a9bbcb8e758ab96120fe9469
OliRamos <oli.ramos@ymail.com>
2023-11-10 17:23:11 -0300

    Subindo algoritimo de calculo IMC

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -6,49 +5,68 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Por favor, digite a altura: ");
+        while (!sc.hasNextDouble()) {
+            System.out.println("Entrada inválida, você deve digitar um número decimal, 1,75 por exxemplo.");
+            sc.next();
+        }
         double altura = sc.nextDouble();
-        System.out.print("Agora por favor, digite seu peso: ");
+        System.out.print("Agora por favor, digite o peso: ");
+        while (!sc.hasNextDouble()) {
+            System.out.println("Entrada inválida, você deve digitar um número decimal, 80 por exxemplo.");
+            sc.next();
+        }
         double peso = sc.nextDouble();
 
         double imc = calculaImc(altura, peso);
-        System.out.printf("Seu IMC é de: %.2f kg/m²\n", imc );
-
         String classificacao = classificaImc(imc);
-        System.out.printf("A classificação de seu IMC é: %s\n", classificacao);
-
         double pesoIdeal = calculaPesoIdeal(altura);
-        System.out.printf("Seu peso ideal é de %.2f kg.", pesoIdeal);
 
-
-
+        String resultadoAdequado = retornaResultado(classificacao, pesoIdeal,imc);
+        System.out.println(resultadoAdequado);
 
     }
+
     public static double calculaImc(double altura, double peso) {
         return peso / (altura * altura);
     }
 
-    public static String classificaImc(double imc){
-            if(imc < 18.5){
-                return "Magreza";
+    public static String classificaImc(double imc) {
+        if (imc < 18.5) {
+            return "Magreza";
+        } else if (imc >= 18.5 && imc < 24.9) {
+            return "Normal";
+        } else if (imc >= 22 && imc < 29.9) {
+            return "Sobrepeso";
+        } else if (imc >= 30.0 && imc < 34.9) {
+            return "Obesidade grau I";
+        } else if (imc >= 35.0 && imc < 39.9) {
+            return "Obesidade grau II";
+        } else {
+            return "Obesidade grau III";
         }
-            else if(imc >= 18.5 && imc < 24.9){
-                return "Normal";
-        }
-            else if (imc >= 22 && imc < 29.9){
-                return "Sobrepeso";
-            } else if (imc >= 30.0 && imc < 34.9) {
-                return "Obesidade grau I";
-            } else if (imc >= 35.0 && imc < 39.9) {
-                return "Obesidade grau II";
-            } else {
-                return "Obesidade grau III";
-        }
-
     }
 
-    public static double calculaPesoIdeal(double altura){
+    public static double calculaPesoIdeal(double altura) {
         return (altura * altura) * 24.9;
     }
 
-
+    public static String retornaResultado(String classificacao, double pesoIdeal, double imc) {
+        if (classificacao.equals("Normal")) {
+            return String.format("\nResultado:\n" +
+                    "Seu IMC é de: %.2f kg/m²\n" +
+                    "Parabéns!\n" +
+                    "De acordo com a OMS, seu IMC é considerado %s.\n" +
+                    "Para manter valor de IMC normal, seu peso deve ser de: %.2f kg."
+                    , imc, classificacao, pesoIdeal);
+        } else {
+            return String.format("\nResultado:\n" +
+                    "Seu IMC é de: %.2f kg/m²\n" +
+                    "Atenção!\n" +
+                    "De acordo com a OMS, seu IMC é considerado %s.\n" +
+                    "Para ter o valor de IMC normal, seu peso deve ser de: %.2f kg.", imc, classificacao, pesoIdeal);
+        }
+    }
 }
+
+
+
